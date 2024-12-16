738e2299ce9c4e4a3620387c9f35ac609d78a0d8
Jeremy Wei <jeremy.t.wei@gmail.com>
2024-12-16 15:17:36 -0500

    # This is a combination of 3 commits. # This is the 1st commit message:
    add sei_getTransactionReceipt
    
    # This is the commit message #2:
    
    add logic to exclude panic txs from getBlockByNumber
    
    # This is the commit message #3:
    
    fixed compilation errs

--- evmrpc/tracers.go
+++ evmrpc/tracers.go
@@ -2,6 +2,7 @@ package evmrpc
 
 import (
 	"context"
+	"encoding/json"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -23,10 +24,32 @@ type DebugAPI struct {
 	connectionType ConnectionType
 }
 
+type SeiDebugAPI struct {
+	*DebugAPI
+	isPanicTx func(ctx context.Context, hash common.Hash) (bool, error)
+}
+
 func NewDebugAPI(tmClient rpcclient.Client, k *keeper.Keeper, ctxProvider func(int64) sdk.Context, txDecoder sdk.TxDecoder, config *SimulateConfig, connectionType ConnectionType) *DebugAPI {
 	backend := NewBackend(ctxProvider, k, txDecoder, tmClient, config)
 	tracersAPI := tracers.NewAPI(backend)
 	return &DebugAPI{tracersAPI: tracersAPI, tmClient: tmClient, keeper: k, ctxProvider: ctxProvider, txDecoder: txDecoder, connectionType: connectionType}
+}
+
+func NewSeiDebugAPI(
+	tmClient rpcclient.Client,
+	k *keeper.Keeper,
+	ctxProvider func(int64) sdk.Context,
+	txDecoder sdk.TxDecoder,
+	config *SimulateConfig,
+	connectionType ConnectionType,
+	isPanicTx func(ctx context.Context, hash common.Hash) (bool, error),
+) *SeiDebugAPI {
+	backend := NewBackend(ctxProvider, k, txDecoder, tmClient, config)
+	tracersAPI := tracers.NewAPI(backend)
+	return &SeiDebugAPI{
+		DebugAPI:  &DebugAPI{tracersAPI: tracersAPI, tmClient: tmClient, keeper: k, ctxProvider: ctxProvider, txDecoder: txDecoder, connectionType: connectionType},
+		isPanicTx: isPanicTx,
+	}
 }
 
 func (api *DebugAPI) TraceTransaction(ctx context.Context, hash common.Hash, config *tracers.TraceConfig) (result interface{}, returnErr error) {
@@ -34,6 +57,40 @@ func (api *DebugAPI) TraceTransaction(ctx context.Context, hash common.Hash, con
 	defer recordMetrics("debug_traceTransaction", api.connectionType, startTime, returnErr == nil)
 	result, returnErr = api.tracersAPI.TraceTransaction(ctx, hash, config)
 	return
+}
+
+func (api *SeiDebugAPI) TraceBlockByNumber(ctx context.Context, number rpc.BlockNumber, config *tracers.TraceConfig) (result interface{}, returnErr error) {
+	startTime := time.Now()
+	defer recordMetrics("debug_traceBlockByNumber", api.connectionType, startTime, returnErr == nil)
+	result, returnErr = api.tracersAPI.TraceBlockByNumber(ctx, number, config)
+	var resultMap map[string]interface{}
+	if err := json.Unmarshal(result.([]byte), &resultMap); err != nil {
+		return nil, err
+	}
+	// iterate over the resultMap and skip any txHash is a panic tx
+	var filteredResult []interface{}
+	for _, txTraceResult := range resultMap {
+		if _, ok := resultMap["error"]; ok {
+			continue
+		}
+		filteredResult = append(filteredResult, txTraceResult)
+	}
+	return filteredResult, nil
+}
+
+func (api *DebugAPI) isPanicTx(ctx context.Context, hash common.Hash) (bool, error) {
+	result, err := api.TraceTransaction(ctx, hash, nil)
+	if err != nil {
+		return false, err
+	}
+	var resultMap map[string]interface{}
+	if err := json.Unmarshal(result.([]byte), &resultMap); err != nil {
+		return false, err
+	}
+	if _, ok := resultMap["error"]; ok {
+		return true, nil
+	}
+	return false, nil
 }
 
 func (api *DebugAPI) TraceBlockByNumber(ctx context.Context, number rpc.BlockNumber, config *tracers.TraceConfig) (result interface{}, returnErr error) {
