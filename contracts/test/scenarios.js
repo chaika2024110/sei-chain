772952a45930a78cf6ed32b88dd24a466a801e6d
Jeremy Wei <jeremy.t.wei@gmail.com>
2025-01-14 15:23:38 -0500

    also deploy erc20 pointer for cw20

--- contracts/test/scenarios.js
+++ contracts/test/scenarios.js
@@ -7,18 +7,20 @@ const { getImplementationAddress } = require('@openzeppelin/upgrades-core');
 const axios = require("axios");
 const { default: BigNumber } = require("bignumber.js");
 const {
-    setupSigners, getAdmin, deployWasm, WASM, createTokenFactoryTokenAndMint
+    setupSigners, getAdmin, deployWasm, WASM, createTokenFactoryTokenAndMint, deployErc20PointerForCw20
 } = require("./lib");
 
 describe("Scenarios", function () {
     let accounts;
     let admin;
     let cw20Address;
+    let pointerAddr;
     let cw20Bank;
     before(async function () {
         accounts = await setupSigners(await hre.ethers.getSigners());
         admin = await getAdmin();
 
+        // cw20
         cw20Address = await deployWasm(WASM.CW20, accounts[0].seiAddress, "cw20", {
             name: "Test",
             symbol: "TEST",
@@ -32,6 +34,9 @@ describe("Scenarios", function () {
                 "minter": admin.seiAddress, "cap": "99900000000"
             }
         });
+
+        // make pointer for cw20
+        pointerAddr = await deployErc20PointerForCw20(hre.ethers.provider, cw20Address)
 
         // native token
         const random_num = Math.floor(Math.random() * 10000)
@@ -47,5 +52,6 @@ describe("Scenarios", function () {
         console.log("cw20 address", cw20Address);
         console.log("native token address", denom);
         console.log("cw20 bank address", cw20Bank);
+        console.log("cw20 pointer address", pointerAddr);
     });
 });
